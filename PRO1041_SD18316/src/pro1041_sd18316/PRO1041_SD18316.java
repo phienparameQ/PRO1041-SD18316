65b037745c81b3d389bfbd17e4d5b7d109366ee2
Trungntph42450 <Trungntph42450@fpt.edu.vn>
2023-11-03 19:08:08 +0700

    init project

---
+++ PRO1041_SD18316/src/pro1041_sd18316/PRO1041_SD18316.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pro1041_sd18316;
+
+/**
+ *
+ * @author Nguyen Thanh Trung
+ */
+public class PRO1041_SD18316 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Trung");
+    }
+    
+}
